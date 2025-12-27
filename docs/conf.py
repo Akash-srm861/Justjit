7ceb3840e504964af3792df78f9240997d9b22dd
magi8101 <mj7841@srmist.edu.in>
2025-12-27 15:49:12 +0530

    changed Copyright

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 # -- Project information -----------------------------------------------------
 
 project = 'JustJIT'
-copyright = '2024, JustJIT Contributors'
+copyright = '2024, Magi-sharma'
 author = 'Magi-sharma'
 release = '0.1.4'
 version = '0.1.4'
